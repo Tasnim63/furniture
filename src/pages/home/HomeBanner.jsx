f6089f3e0e2522d8dba0682f7a96d329ae5057d4
tamanna <tamanna202163@gmail.com>
2022-09-20 11:53:34 +0600

    home banner

--- src/pages/home/HomeBanner.jsx
+++ src/pages/home/HomeBanner.jsx
@@ -3,9 +3,13 @@ import background from '../../images/homebanner.jpg'
 export default function HomeBanner() {
   return (
    <>
-   <div className=' bg-cover bg-no-repeat bg-center ' style={{ backgroundImage: `url(${background})`, height:"90vh" }}>
+   <div className=' bg-cover bg-no-repeat bg-center  py-36' style={{ backgroundImage: `url(${background})` }}>
 <div className="container">
-  <h1 className=' text-6xl text-white capitalize '>Seasons Sale <br/>2022!</h1>
+  <h1 className=' text-6xl text-blue capitalize '>Seasons Sale <br/><span className=' text-red '>2022!</span></h1>
+  <p className=' text-white my-4'>Natural Log Dining Table Chair : Pellentesque habitant morbi tristique<br/> senectus et netus et malesuada fames ac turpis egestas. Vestib</p>
+  <div className=' mt-12'>
+<a className=' bg-white text-blue border border-blue px-8 py-3 font-medium rounded-md hover:bg-transparent hover:text-red hover:border-red' href="#">Shop Now</a>
+  </div>
 </div>
    </div>
    </>
