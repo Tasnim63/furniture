eeb068834d978029b240c895d08ae97d523b4848
tamanna <tamanna202163@gmail.com>
2022-09-20 19:00:40 +0600

    product cart added

--- src/pages/home/ProductCard.jsx
+++ src/pages/home/ProductCard.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import product1 from '../../images/product1'
 export default function ProductCard() {
   return (
 <>
 <div className=' container'>
 <h1 className='text-2xl font-medium text-gray uppercase mb-6'>Our Products</h1>
+{/* product grid */}
+
+<div className="grid grid-cols-4 gap-6">
+{/* single product */}
+<div className="bg-white shadow-md rounded overflow-hidden">
+<div className=' relative'>
+<img src={product1} alt="" />
+</div>
+</div>
+</div>
+
+
 </div>
 </>
   )
